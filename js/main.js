b87a463f44b70ba249203ead39ff5c41a144a5fb
BondarchukM1 <72317908+BondarchukM1@users.noreply.github.com>
2020-10-09 15:52:31 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
-const cartButton =document.querySelector(".cart-button");
+const cartButton = document.querySelector(".cart-button");
+
 const modal = document.querySelector(".modal");
 const close = document.querySelector(".close");
+const closethis = document.querySelector(".close-this");
 cartButton.addEventListener("click",toggleModal);
+
 close.addEventListener("click",toggleModal);
+closethis.addEventListener("click",toggleModal);
 
 function toggleModal() {
   modal.classList.toggle("is-open");
